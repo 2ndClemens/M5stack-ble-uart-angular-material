4c3092d933394d363269f71bca623b5c01925be8
clemens.poblotzki <clemens.poblotzki@centigrade.de>
2021-03-17 00:19:46 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,117 @@
+/// <reference types="web-bluetooth" />
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'm5stack-ble';
+  myCharacteristic: any;
+  async onButtonClick() {
+    let serviceUuid = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
+    let characteristicUuidTx = '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
+    let characteristicUuidRx = '6e400003-b5a3-f393-e0a9-e50e24dcca9e';
+    let device = null;
+
+    try {
+      console.log('Requesting Bluetooth Device...');
+      device = await navigator.bluetooth.requestDevice({
+        filters: [{ services: [serviceUuid] }, { name: 'm5-stack' }],
+      });
+
+      console.log('Connecting to GATT Server...');
+      const server = await device.gatt?.connect();
+
+      console.log('Getting Service...');
+      const service = await server?.getPrimaryService(serviceUuid);
+
+      console.log('Getting Characteristics...');
+      const characteristics = await service?.getCharacteristics(
+        characteristicUuidTx
+      );
+
+      if (characteristics && characteristics.length > 0) {
+        this.myCharacteristic = characteristics[0];
+
+        /* console.log('Reading Characteristics...');
+        const value = await myCharacteristic.readValue();
+        const decoder = new TextDecoder('utf-8');
+        console.log(decoder.decode(value)); */
+
+        const encoder = new TextEncoder();
+        const text = 'on';
+        await this.myCharacteristic.writeValue(encoder.encode(text));
+
+        /* await myCharacteristic.startNotifications();
+        myCharacteristic.addEventListener('characteristicvaluechanged', (event) => {
+          const value = event.target.value;
+          const decoder = new TextDecoder('utf-8');
+          console.log(decoder.decode(value));
+        });
+  
+        console.log('Waiting 60 seconds to receive data from the device...')
+        await sleep(600 * 1000); */
+      }
+
+      console.log('Getting Characteristics...');
+      const characteristicsRx = await service?.getCharacteristics(
+        characteristicUuidRx
+      );
+
+      if (characteristicsRx && characteristicsRx.length > 0) {
+        const myCharacteristic = characteristicsRx[0];
+
+        /* console.log('Reading Characteristics...');
+        const value = await myCharacteristic.readValue();
+        const decoder = new TextDecoder('utf-8');
+        console.log(decoder.decode(value)); */
+
+        /*       const encoder = new TextEncoder("utf-8");
+        const text = "on";
+        await myCharacteristic.writeValue(encoder.encode(text)); */
+
+        await myCharacteristic.startNotifications();
+        myCharacteristic.addEventListener(
+          'characteristicvaluechanged',
+          (event: any) => {
+            const value = event.target.value;
+            const decoder = new TextDecoder('utf-8');
+            console.log(decoder.decode(value));
+          }
+        );
+
+        console.log('Waiting 60 seconds to receive data from the device...');
+        await this.sleep(600 * 1000);
+      }
+    } catch (error) {
+      console.log('Argh! ' + error);
+    }
+
+    if (device) {
+      if (device.gatt?.connected) {
+        device.gatt.disconnect();
+        console.log('disconnect');
+      }
+    }
+  }
+
+  async sleep(ms: any) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(0);
+      }, ms);
+    });
+  }
+  async buttonOn() {
+    const encoder = new TextEncoder();
+    const text = 'on';
+    await this.myCharacteristic.writeValue(encoder.encode(text));
+  }
+
+  async buttonOff() {
+    const encoder = new TextEncoder();
+    const text = 'off';
+    await this.myCharacteristic.writeValue(encoder.encode(text));
+  }
 }
